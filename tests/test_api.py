c0816fa317a4d1716260da80648cc93766f1fab9
ford <fordwoof9@outlook.com>
2021-10-26 18:55:11 +0000

    Add retrieve all beasts API

---
+++ tests/test_api.py
@@ -0,0 +1,18 @@
+import pytest
+import requests
+
+url = "http://forddeza2545.pythonanywhere.com"
+
+def test_retrieve_all():
+    response = requests.get(url + '/v1/beast')
+    assert response.status_code == 200
+
+def test_retrieve_count_all():
+    response = requests.get(url + '/v1/beast')
+    response_body = response.json()
+    assert len(response_body) == 3
+
+def test_retrieve_all_niffler():
+    response = requests.get(url + '/v1/beast')
+    response_body = response.json()
+    assert response_body["Niffler"]["name"] == "Niffler"
