7801d14302f986b85adf4d34ae63267d58fcfb1f
ford <fordwoof9@outlook.com>
2021-11-02 12:03:17 +0000

    add /checkdb

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,6 +3,15 @@ import requests
 
 url = "http://forddeza2545.pythonanywhere.com"
 
+def test_connection():
+    response = requests.get(url)
+    assert response.status_code == 200
+
+def test_checkdb():
+    response = requests.get(url + '/v1/checkdb')
+    response_body = response.json()
+    assert response_body["Database connection"] [0] ["1"] == 1
+
 def test_retrieve_all():
     response = requests.get(url + '/v1/beast')
     assert response.status_code == 200
